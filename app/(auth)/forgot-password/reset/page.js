248eb6627b3132a8f79c381a2022596a8527dbf7
abdullahmia <abdullah149161@bsdi-bd.org>
2024-07-16 16:09:47 +0600

    fix: resolved build error

--- app/(auth)/forgot-password/reset/page.js
+++ app/(auth)/forgot-password/reset/page.js
@@ -1,4 +1,5 @@
 import { GenerateBreadcrumb } from "@/components/generate-breadcrumb";
+import { Suspense } from "react";
 import ResetPasswordForm from "./components/reset-password-form";
 
 export const metadata = {
@@ -6,14 +7,16 @@ export const metadata = {
   description: "Reset Password to your account",
 };
 
-export default function ForgotPassword() {
+export default function ResetPassword() {
   return (
     <div>
       <GenerateBreadcrumb title="Reset Password" />
 
       <div className="container w-full h-full flex flex-col items-center justify-center space-y-5 lg:py-32 py-14">
         {/* Reset password form */}
-        <ResetPasswordForm />
+        <Suspense>
+          <ResetPasswordForm />
+        </Suspense>
       </div>
     </div>
   );
