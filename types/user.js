5556c204070429f4c8bc633d46423060a21c4ac7
abdullahmia <abdullah149161@bsdi-bd.org>
2024-08-27 23:40:44 +0600

    fix: update password rule

--- types/user.js
+++ types/user.js
@@ -1,13 +1,14 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 export const ZCreateUser = z.object({
   name: z
-    .string({ message: "Name is required" })
-    .min(3, { message: "Name should be at least 3 characters long" }),
+    .string({ message: 'Name is required' })
+    .min(3, { message: 'Name should be at least 3 characters long' }),
   email: z.string().email(),
   password: z
     .string()
-    .min(6, { message: "Password should be at least 6 characters long" }),
+    .min(6, { message: 'Password should be at least 6 characters long' })
+    .trim(),
   // password: z.string()
   // .min(8, "Password must be at least 8 characters long")
   // .trim()
@@ -20,8 +21,8 @@ export const ZCreateUser = z.object({
 export const ZUser = z.object({
   id: z.string(),
   name: z
-    .string({ message: "Name is required" })
-    .min(3, { message: "Name should be at least 3 characters long" }),
+    .string({ message: 'Name is required' })
+    .min(3, { message: 'Name should be at least 3 characters long' }),
   email: z.string().email(),
   password: z.string(),
   address: z.object({
@@ -32,9 +33,9 @@ export const ZUser = z.object({
     country: z.string(),
   }),
   phone: z.string(),
-  photo: z.string().url({ message: "Photo must be a valid URL" }),
-  role: z.enum(["user", "admin"]),
-  identityProvider: z.enum(["email", "google"]),
+  photo: z.string().url({ message: 'Photo must be a valid URL' }),
+  role: z.enum(['user', 'admin']),
+  identityProvider: z.enum(['email', 'google']),
   createdAt: z.date(),
   updatedAt: z.date(),
 });
